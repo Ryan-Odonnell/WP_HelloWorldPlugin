1d71d407c183b3ea0dbf08f253934fd99da114e7
Ryan O'Donnell <ryan.odonnell@paymentsense.com>
2016-08-18 15:13:13 +0100

    initial project version

---
+++ helloworld.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Plugin Name: Hello World Plugin
+* Plugin URI: http://www.psdevsupport.co.uk
+* Description: Hello World plugin for WordPress
+* Version: 1.0.0
+* Author: Ryan O'Donnell
+* Author URI: http://www.psdevsupport.com
+* License: GPL2
+*/
+
+?>
